2a6ff09123a36f07ab8308a6685eaffd72c3e9d9
Dinh Viet Anh <dv20iu54@student.bmstu.ru>
2023-02-11 01:12:43 +0300

    Update

--- lab_01/main.c
+++ lab_01/main.c
@@ -5,41 +5,60 @@
 #include <errno.h>
 #include <unistd.h>
 #include <wait.h>
-#define STR1 "How are you?"
-#define STR2 "I'm ok, thank you."
+#define STR1 "How are you from child 1?"
+#define STR2 "How are you from child 2?"
+#define ANSWER1 "I'm ok, thank you to child 1."
+#define ANSWER2 "I'm ok, thank you to child 2."
 #define BUF_SIZE 1024
 int main(int argc, char ** argv)
 { 
+    // массив файловых дескрипторов: файловые дескрипторы
     int sockets[2];
+
+    // Буфер для получения сообщения
     char buf[BUF_SIZE];
-    int pid;
-    if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) < 0) 
+
+    // сообщение от дочерних процессов
+    char message[2][BUF_SIZE] = {STR1, STR2};
+
+    // сообщение от родительского процесса
+    char answer[2][BUF_SIZE] = {ANSWER1, ANSWER2};
+    int children[2];
+    for (int i = 0; i < 2; i++)
     {
-        perror("socketpair() failed.\n");
-        return EXIT_FAILURE;
-    }
-    pid = fork();
-    if (pid == - 1)
-    {
-        perror("fork() failed.\n");
-        return EXIT_FAILURE;
-    }
-    if (pid == 0) 
-    {
-        close(sockets[1]);
-        printf("Child sent message.\n");
-        write(sockets[0], STR1, sizeof(STR1));
-        read(sockets[0], buf, sizeof(buf));
-        printf("Child received message: %s\n", buf);
-        close(sockets[0]);
+        // Создать парные сокеты 
+        if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) < 0) 
+        {
+            perror("socketpair() failed.\n");
+            return EXIT_FAILURE;
+        }
+        children[i] = fork();
+        if (children[i] == -1)
+        {
+            perror("fork() failed.\n");
+            return EXIT_FAILURE;
+        }
+        // Если это дочерный процесс
+        if (children[i] == 0) 
+        {
+            close(sockets[1]);
+            printf("Child %d sent message.\n", i + 1);
+            write(sockets[0], message[i], sizeof(message[i]));
+            read(sockets[0], buf, sizeof(buf));
+            printf("Child %d received message: %s\n", i + 1, buf);
+            close(sockets[0]);
+            exit(0); // Завершить дочерный процесс
+        } 
+        // родительский процесс
+        else 
+        {
+            close(sockets[0]);
+            read(sockets[1], buf, sizeof(buf));
+            printf("Parents received message: %s\n", buf);
+            printf("Parents sent message.\n");
+            write(sockets[1], answer[i], sizeof(answer[i]));
+            close(sockets[1]); 
+        }
     } 
-    else 
-    {
-        close(sockets[0]);
-        read(sockets[1], buf, sizeof(buf));
-        printf("Parents received message: %s\n", buf);
-        printf("Parents sent message.\n");
-        write(sockets[1], STR2, sizeof(STR2));
-        close(sockets[1]);
-    }
+    return 0;
 }
