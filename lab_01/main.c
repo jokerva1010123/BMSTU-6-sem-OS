6927ccd5787b5f972e818f3cf10e3bc0feb7c841
Dinh Viet Anh <dv20iu54@student.bmstu.ru>
2023-02-11 00:17:44 +0300

    Update

---
+++ lab_01/main.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <wait.h>
+#define STR1 "How are you?"
+#define STR2 "I'm ok, thank you."
+#define BUF_SIZE 1024
+int main(int argc, char ** argv)
+{ 
+    int sockets[2];
+    char buf[BUF_SIZE];
+    int pid;
+    if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) < 0) 
+    {
+        perror("socketpair() failed.\n");
+        return EXIT_FAILURE;
+    }
+    pid = fork();
+    if (pid == - 1)
+    {
+        perror("fork() failed.\n");
+        return EXIT_FAILURE;
+    }
+    if (pid == 0) 
+    {
+        close(sockets[1]);
+        printf("Child sent message.\n");
+        write(sockets[0], STR1, sizeof(STR1));
+        read(sockets[0], buf, sizeof(buf));
+        printf("Child received message: %s\n", buf);
+        close(sockets[0]);
+    } 
+    else 
+    {
+        close(sockets[0]);
+        read(sockets[1], buf, sizeof(buf));
+        printf("Parents received message: %s\n", buf);
+        printf("Parents sent message.\n");
+        write(sockets[1], STR2, sizeof(STR2));
+        close(sockets[1]);
+    }
+}
